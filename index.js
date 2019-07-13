881100971ec16dd0ac2bbfe98dce42e5656545f9
Vlad Nitu <vpnitu@edu.uwaterloo.ca>
2019-07-13 18:05:29 -0400

    Finished migration of logic to server side

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const chess = require('./chess');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -9,55 +10,78 @@ const app = express();
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 
-let game = new Game();
-let colour = WHITE;
+let game = new chess.Game();
+let colour = chess.WHITE;
 
 app.post('/getPiece', (req, res, next) => {
-   let piece = game.board[req.body.row][req.body.col];
-   res.send(piece);
+  let piece = game.board[req.body.row][req.body.col];
+  if (piece !== null) {
+    res.json({name: piece.constructor.name, colour: (piece.colour === chess.WHITE ? "white" : "black")})
+  } else {
+    res.json(piece);
+  }
 });
-app,post('/selectMove', (req, res, next) => {
+app.post('/selectMove', (req, res, next) => {
   let playerPieces = game.pieces[colour];
-  let piece, moves, move, from, to;
 
-  while (true) {
-    let king = playerPieces.find(piece => piece instanceof King);
-    moves = king.possibleMoves();
-    let castle = moves.find(moves => move === QUEENSIDE || moves === KINGSIDE);
-    if (castle !== undefined) {
-        move = castle;
-        break;
+  let piece = playerPieces.find(piece => piece instanceof chess.King);
+  let moves = piece.possibleMoves();
+  let move = moves.find(move => move === chess.QUEENSIDE || move === chess.KINGSIDE);
+
+  if (move === undefined) {
+    while (true) {
+      piece = playerPieces[Math.floor(Math.random() * playerPieces.length)];
+      moves = piece.possibleMoves();
+
+      if (moves.length > 0) {
+          move = moves[Math.floor(Math.random() * moves.length)];
+          res.send({from: {row: piece.pos.row, col: piece.pos.col}, to: move});
+          return;
+      }
     }
-    piece = playerPieces[Math.floor(Math.random() * playerPieces.length)];
-    moves = piece.possibleMoves();
-
-    if (moves.length > 0) {
-        move = moves[Math.floor(Math.random() * moves.length)];
-        break;
+  } else {
+    if (move === chess.KINGSIDE){
+      res.send({from: {row: piece.pos.row, col: piece.pos.col}, to: {row: piece.pos.row, col: piece.pos.col + 2}});
+    } else {
+      res.send({from: {row: piece.pos.row, col: piece.pos.col}, to: {row: piece.pos.row, col: piece.pos.col - 2}});
     }
   }
-
-  if (move !== KINGSIDE && move !== QUEENSIDE) {
-    move = {from: {row: piece.pos.row, col:piece.pos.col}, to: move};
-  }
-
-  res.send(move);
 });
 app.post('/executeMove', (req, res, next) => {
+  console.log(req.body);
   try {
-    if (req.body.move === KINGSIDE || req.body.move === QUEENSIDE) {
-        game.tryCastle(colour, req.body.move);
-    } else {
-        game.tryMove(req.body.move.from, req.body.move.to);
-    }
-    colour = (colour + 1) % 2;
+    game.tryMove(req.body.move);
   } catch (error) {
-    if (error instanceof InvalidMoveError) {
-      res.send(error);
+    if (error instanceof chess.InvalidMoveError) {
+      res.send({error: error.message});
+      return;
     } else {
       throw error;
     }
   }
+  game.print();
+  colour = (colour + 1) % 2;
+  if (game.isMate(colour)) {
+    res.send({result: (colour === chess.WHITE ? "Black" : "White") + " Wins!"});
+    return;
+  }
+  if (game.isStalemate(colour)) {
+    res.send({result: "Stalemate"});
+    return;
+  }
+  if (game.fiftyMoveDraw) {
+    res.send({result: "50 Move Draw"});
+    return;
+  }
+  if (game.isRepetition()) {
+    res.send({result: "Repetition"});
+    return;
+  }
+  if (game.isInsufficientMaterial()) {
+    res.send({result: "Insufficient Material"});
+    return;
+  }
+  res.send({});
 });
 
 const server = http.createServer(app);
